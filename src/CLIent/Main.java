a836fa4537bc6309e0a54d00d5ed80a7dba18018
Nicolò Vescera <nicolo.vescera@gmail.com>
2017-02-02 21:32:58 +0100

    Aggiunti parametri al main

--- src/CLIent/Main.java
+++ src/CLIent/Main.java
@@ -17,11 +17,24 @@ public class Main {
      */
     
     public static void main(String[] args) {
-        CLIent client = new CLIent("localhost",2000);
+        String host;
+        int port;
+        
+        if(args.length > 0){
+            host = args[0];
+            port = Integer.parseInt(args[1]);
+        }
+        else{
+            host = "localhost";
+            port = 2000;
+        }
+            
+        CLIent client = new CLIent(host,port);
         
         while(true){
             client.sendMessage(EasyInput.inputS(""));
         }
+        //System.out.println("\033[31;1mHello\033[0m, \033[32;1;2mworld!\033[0m");//colora l'output
         
     }
     
